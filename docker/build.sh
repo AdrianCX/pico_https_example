04ef22d952780a3a7b53afcdc526d251bbc89b30
Adrian Cruceru <cruceruadrian@gmail.com>
2024-09-13 19:46:10 +0200

    Initial commit

---
+++ docker/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+if [ ! -d "/source/build/picotool" ]; then
+    mkdir -p /source/build/picotool
+fi
+
+if [ ! -d "/source/build/docker" ]; then
+    mkdir -p /source/build/docker
+fi
+
+if [ ! -d "/source/build/test" ]; then
+    mkdir -p /source/build/test
+fi
+
+cd /source/build/picotool
+cmake -DPICO_SDK_PATH=/source/pico_https/pico-sdk/ /source/pico_https/picotool/
+make
+make install
+
+cd /source/build/docker
+cmake -DCMAKE_BUILD_TYPE=Release VERBOSE=1 /source/
+make
+
+cd /source/build/test
+cmake -DCMAKE_BUILD_TYPE=Release VERBOSE=1 /source/pico_https/test/
+make
+ctest --verbose
